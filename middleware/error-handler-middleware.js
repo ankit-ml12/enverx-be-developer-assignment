5c02ae509cb2addbb0507b7d4fa2de538e304eea
ankit patidar <patiankit6555@gmail.com>
2023-08-03 11:41:10 +0530

    mongoose error via error handler

--- middleware/error-handler-middleware.js
+++ middleware/error-handler-middleware.js
@@ -1,6 +1,26 @@
 const { StatusCodes } = require('http-status-codes')
 const erroHandlerMiddleware = (err, req, res, next) => {
-  console.log('err:', err)
-  res.status(StatusCodes.BAD_REQUEST).json({ err })
+  let customError = {
+    //set Default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again leter',
+  }
+  if (err.name == 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',')
+    customError.statusCode = 400
+  }
+  if (err.name == 'CastError') {
+    customError.msg = `No item found with id ${err.value}`
+    customError.statusCode = 404
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, Please choose another one`
+    customError.statusCode = 404
+  }
+  return res.status(customError.statusCode).json({ err: customError.msg })
 }
 module.exports = erroHandlerMiddleware
