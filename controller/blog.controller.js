837828383830dd1b65bdab814ec2c99781fdfed2
ankit patidar <patiankit6555@gmail.com>
2023-08-03 10:16:47 +0530

    crete Blog

--- controller/blog.controller.js
+++ controller/blog.controller.js
@@ -1,11 +1,23 @@
-const getAllblog = async (req, res) => {
-  res.send('get All blog')
-}
+const Blog = require('../model/blog.model')
+const { StatusCodes } = require('http-status-codes')
+
+const getAllblog = async (req, res) => {}
 const getOneBlog = async (req, res) => {
   res.send('get one blog')
 }
 const createBlog = async (req, res) => {
-  res.send('create blog')
+  const blogData = req.body
+
+  //creating blog using Blog schema
+  const blog = await Blog.create(blogData)
+
+  const blogId = blog._id.toString()
+  //adding url into the blog
+  blog.url = `/blogs/${blogId}`
+
+  await blog.save()
+  //return the result
+  await res.status(StatusCodes.OK).json({ blog })
 }
 const updateBlog = async (req, res) => {
   res.send('update blog')
--- middleware/error-handler-middleware.js
+++ middleware/error-handler-middleware.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require('http-status-codes')
 const erroHandlerMiddleware = (err, req, res, next) => {
-  res.StatusCodes(StatusCodes.BAD_REQUEST).json({ err })
+  res.status(StatusCodes.BAD_REQUEST).json({ err })
 }
 module.exports = erroHandlerMiddleware
